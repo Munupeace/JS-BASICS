db7f6ad049a4a9617bdd5fcf7ad79b6ca9c02f62
Abel Olije <abelolije@gmail.com>
2024-05-29 20:44:56 +0100

    portfolio

--- js/bank2.js
+++ js/bank2.js
@@ -1,9 +1,11 @@
 function showDropdown(){
     document.getElementById("myDropdown").classList.toggle("show");
+    console.log('I was clicked');
 }
 window.onclick = function(e) {
     if (!e.target.matches('#clicked')) {
-    var myDropdown = document.getElementById("myDropdown");   
+    var myDropdown = document.getElementById("myDropdown");
+    console.log(myDropdown);   
       if (myDropdown.classList.contains('show')) {
         myDropdown.classList.add('show');
       }else{
