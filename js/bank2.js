970fc0c0e06612ad3b05d9ec61e0d9ec222edeb2
Abel Olije <abelolije@gmail.com>
2024-06-20 13:40:27 +0100

    dropdown-task

--- js/bank2.js
+++ js/bank2.js
@@ -1,15 +1,23 @@
-function showDropdown(){
-    document.getElementById("myDropdown").classList.toggle("show");
-    console.log('I was clicked');
+const menuItems = document.getElementById('myDropdown');
+const barsIcon = document.getElementById('bars-icon');
+const showDropdown = document.getElementById('clicked')
+
+barsIcon.onclick = () =>{
+menuItems.classList.toggle('show')
+
 }
-window.onclick = function(e) {
-    if (!e.target.matches('#clicked')) {
-    var myDropdown = document.getElementById("myDropdown");
-    console.log(myDropdown);   
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.add('show');
-      }else{
-        myDropdown.classList.remove('show')
-      }
-    }
-  }
+// function showDropdown(){
+//     document.getElementById("myDropdown").classList.toggle("show");
+//     console.log('I was clicked');
+// }
+// window.onclick = function(e) {
+//     if (!e.target.matches('#clicked')) {
+//     var myDropdown = document.getElementById("myDropdown");
+//     console.log(myDropdown);   
+//       if (myDropdown.classList.contains('show')) {
+//         myDropdown.classList.add('show');
+//       }else{
+//         myDropdown.classList.remove('show')
+//       }
+//     }
+//   }
